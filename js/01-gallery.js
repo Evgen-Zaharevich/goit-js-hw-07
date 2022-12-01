79e345ae9141d142f7fdaf977f1ac8c6ed451e90
Evgen Zaharevich <metrovyy@gmail.com>
2022-12-01 17:03:48 +0200

    chek

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createImageMarkup(markups) {
+  return markups
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+              class="gallery__image"
+              src="small-image.jpg"
+              data-source="large-image.jpg"
+              alt="Image description"
+            />
+          </a>
+        </div>`
+    )
+    .join("");
+}
+
+console.log(createImageMarkup(galleryItems));
