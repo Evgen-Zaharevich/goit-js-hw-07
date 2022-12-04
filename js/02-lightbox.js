bb7d5ee3a90bbcbae9f2fc658b19eaf272526fcd
Evgen Zaharevich <metrovyy@gmail.com>
2022-12-04 18:44:54 +0200

    2 task finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createImageMarkup(markups) {
+  return markups
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createImageMarkup(galleryItems)
+);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: `bottom`,
+  captionDelay: 250,
+  fadeSpeed: 350,
+});
